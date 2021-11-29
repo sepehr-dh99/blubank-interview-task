1d80a5c9370f8497923e81dd82506663b3269450
sepehr-dh99 <sepehr.dh99@gmail.com>
2021-11-29 18:43:31 +0330

    handeling infinite scroll

--- src/Pages/Transactions/Transactions.js
+++ src/Pages/Transactions/Transactions.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import "./Transactions.css";
 import useTransactions from "../../Hooks/useTransactions";
 import TransactionBox from "../../Components/TransactionBox";
@@ -7,11 +7,46 @@ export default function Transactions() {
   const [pageNumber, setPageNumber] = useState();
   const { loading, transactions, hasMore } = useTransactions(pageNumber);
 
+  // useRef observer
+  const observer = useRef();
+
+  // callback on observer detections
+  const lastTransaction = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   return (
     <>
-      {transactions.map((transaction) => {
-        return <TransactionBox data={transaction} />;
+      {transactions.map((transaction, index) => {
+        if (transactions.length === index + 1) {
+          return (
+            <div
+              className="transaction-box"
+              ref={lastTransaction}
+              key={transaction.id}
+            >
+              <TransactionBox data={transaction} />
+            </div>
+          );
+        } else {
+          return (
+            <div className="transaction-box" key={transaction.id}>
+              <TransactionBox data={transaction} />
+            </div>
+          );
+        }
       })}
+      {loading && "loading..."}
     </>
   );
 }
